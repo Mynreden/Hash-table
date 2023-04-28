c0addc18248acb224f2aa074855a894bbb58a2b5
Sultan A <aubakirov2004sultan@gmail.com>
2023-04-28 22:27:49 +0600

    added get, remove methods

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -1,3 +1,5 @@
+import java.security.KeyException;
+
 public class MyHashTable<K, V> {
     private static class HashNode<K, V>{
         private final K key;
@@ -14,7 +16,7 @@ public class MyHashTable<K, V> {
             return "{" + key + " " + value + "}";
         }
     }
-    private final HashNode[] chainArray;
+    private final HashNode<K, V>[] chainArray;
     private int M = 11;
     private int size = 0;
 
@@ -32,23 +34,48 @@ public class MyHashTable<K, V> {
     }
 
     public void put(K key, V value){
-        this.size++;
+        this.size ++;
         int index = hash(key);
-        HashNode temp = new HashNode<>(key, value);
+        HashNode<K, V> temp = new HashNode<>(key, value);
         if (this.chainArray[index] == null){
             this.chainArray[index] = temp;
             return;
         }
-        HashNode pointer = this.chainArray[index];
+        HashNode<K, V> pointer = this.chainArray[index];
         while (pointer.next != null){
             pointer = pointer.next;
         }
         pointer.next = temp;
     }
 
-    public V get(K key){}
+    public V get(K key) throws Exception {
+        int index = hash(key);
+        HashNode<K, V> pointer = this.chainArray[index];
+        while (pointer != null && !pointer.key.equals(key)){
+            pointer = pointer.next;
+        }
+        if (pointer == null) throw new Exception("Key does not exist");
+        return pointer.value;
+    }
 
-    public V remove(K key){}
+    public V remove(K key) throws Exception {
+        this.size --;
+        int index = hash(key);
+        HashNode<K, V> pointer = this.chainArray[index];
+        HashNode<K, V> previous = null;
+        while (pointer != null && !pointer.key.equals(key)){
+            previous = pointer;
+            pointer = pointer.next;
+        }
+        if (pointer == null) throw new Exception("Key does not exist");
+        if (previous == null) {
+            this.chainArray[index] = pointer.next;
+        }
+        else{
+            previous.next = pointer.next;
+        }
+        return pointer.value;
+    }
 
     public boolean contains(V value){}
 
